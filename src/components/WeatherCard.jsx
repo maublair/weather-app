b8d363bc8296aae39fd364c83ebffcc9d9908028
maublair <blairfarah@gmail.com>
2022-09-30 17:32:23 -0500

    estilo v0.9

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -6,24 +6,29 @@ const WeatherCard = ({ weather, temperature }) => {
   console.log(temperature)
   return (
     <article className='card'>
-      <h1>Weather App</h1>
-      <h2>{`${weather?.name}, ${weather?.sys.country}`}</h2>
-      <section>
-        <img src={weather ? `https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png` : ''} alt='' />
+      <section className='weather__title'>
+        <h1>Weather App</h1>
+        <h2>{`${weather?.name}, ${weather?.sys.country}`}</h2>
+      </section>
+      <section className='img__weather'>
+        <img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`} alt='' />
         <h3>Temperature</h3>
       </section>
-      <section>
+      <section className='first__data'> {/* in 'first data' the climate data of the geolacized city is shown */}
         <h3>"{weather?.weather[0].description}"</h3>
+        if(Weather)
         <ul>
           <li><span>Wind Speed</span> {weather?.wind.speed}m/s</li>
           <li><span>Clouds</span> {weather?.clouds.all}%</li>
           <li><span>Pressure</span> {weather?.main.pressure}hPa</li>
         </ul>
       </section>
-      <h2>{ isCelsius ? `${temperature?.celsius} Cº` : `${temperature?.farenheit} Fº`}</h2>
-      <button onClick={changeTemperatures}>
-        {isCelsius ? 'Change to Fº' : 'Change to Cº'}
-      </button>
+      <section className='temperature__data'>
+        <h2>{isCelsius ? `${temperature?.celsius} Cº` : `${temperature?.farenheit} Fº`}</h2>
+        <button onClick={changeTemperatures}>
+          {isCelsius ? 'Change to Fº' : 'Change to Cº'}
+        </button>
+      </section>
     </article>
   )
 }
